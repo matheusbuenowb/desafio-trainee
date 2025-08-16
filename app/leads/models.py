a72daefe0bec7aee565a48e365993411c979beb7
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-08-16 11:07:37 -0300

    Modelo básico em models.py, com nome, email e data de criação

--- app/leads/models.py
+++ app/leads/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Lead(models.Model):
+    nome = models.CharField(max_length=100) #nome com limit max de 100
+    email = models.EmailField(unique=True) #campo tipo email, deve ser único
+    criado_em = models.DateTimeField(auto_now_add=True) #data criacao
+
+    def __str__(self):
+        return f"{self.nome} <{self.email}>"
