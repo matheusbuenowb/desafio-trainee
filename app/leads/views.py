4dd2e726c2907945f5ae7d70c531753ccb5c5a2a
Matheus Bueno <matheus.faria_wb@hotmail.com>
2025-08-16 15:41:07 -0300

    Criada uma exceção para erro de emails repetidos e colocado um front em lead_form para isso

--- app/leads/views.py
+++ app/leads/views.py
@@ -2,11 +2,24 @@ import os
 import requests
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.db import IntegrityError
 from .forms import LeadForm
+from .models import Lead
 
 def lead_form(request):
     if request.method == "POST": #verifica se o request é por meio do método POST
+        nome = request.POST.get("first_name") #pega o primeiro nome
+        email = request.POST.get("email") #pega o email
         form = LeadForm(request.POST) #se sim, chamamos leadform e enviamos a requisição
+        Lead.objects.create(first_name = nome, email = email)
+
+        try:
+            Lead.objects.create(first_name = nome, email = email)
+            return redirect("lead_form")
+        except IntegrityError:
+            error = "Este e-mail já está cadastrado"
+
+
         if form.is_valid(): #se for válido, salvamos
             lead = form.save()
 
@@ -21,13 +34,13 @@ def lead_form(request):
                     )
                 except requests.RequestException:
                     messages.error(request, "Erro ao enviar para o n8n")
-
+            
             messages.success(request, "Lead cadastrada com sucesso!")
             return redirect("lead_form") #se der certo, cadastramos a lead
     else:
         form = LeadForm()
 
-    return render(request, "leads/lead_form.html", {"form": form}) #retorna o request e o form
+    return render(request, "leads/lead_form.html", {"error": error}) #retorna o request e
 
 '''
 def lead_form(request):
